fec183fca374a48d0a95439d8d561c98750d13dd
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-12-25 14:52:56 -0200

    Adicionando Evento Click

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -5,6 +5,7 @@ class CalcController{
         this._dateEl = document.querySelector('#data');
         this._timeEl = document.querySelector('#hora');
         this.initialize();
+        this.initButtonsEvents();
     }
     initialize(){
         this.setDisplayDateTime();
@@ -13,8 +14,12 @@ class CalcController{
         },1000)
     }
     initButtonsEvents(){
-        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
-        return buttons;
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+        buttons.forEach( (item, index) =>{
+            item.addEventListener('click', ()=>{
+                console.log(item.className.baseVal.replace('btn-',''));
+            });
+        });
     }
     setDisplayDateTime(){
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
@@ -30,21 +35,21 @@ class CalcController{
     set displayDate(value){
         this._dateEl.innerHTML = value;
     }
-
+    /*--*/ 
     get displayTime(){
         return this._timeEl.innerHTML;
     }
     set displayTime(value){
         this._timeEl.innerHTML = value;
     }
-
+    /*--*/
     get displayCalc(){
         return this._displayCalcEl.innerHTML;
     }
     set displayCalc(value){
         this._displayCalcEl.innerHTML = value;
     }
-
+    /*--*/
     get currentDate(){
         return new Date();
     }
