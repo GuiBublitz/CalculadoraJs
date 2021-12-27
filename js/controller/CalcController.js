d44533532569de3a6635594e66b5301521d0ee25
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2021-12-27 00:05:55 -0200

    Ctrl + c e Ctrl + v

--- js/controller/CalcController.js
+++ js/controller/CalcController.js
@@ -10,6 +10,21 @@ class CalcController{
         this.initialize();
         this.initButtonsEvents();
         this.initKeyBoard();
+        this.pasteFromClipBoard();
+    }
+    pasteFromClipBoard(){
+        document.addEventListener('paste', e =>{
+            let text = e.clipboardData.getData('Text');
+            this.displayCalc = parseFloat(text);
+        });
+    }
+    copyToClickBoard(){
+        let input = document.createElement('input');
+        input.value = this.displayCalc;
+        document.body.appendChild(input);
+        input.select(); 
+        document.execCommand('Copy');
+        input.remove();
     }
     initialize(){
         this.setDisplayDateTime();
@@ -53,6 +68,9 @@ class CalcController{
                 case '8': 
                 case '9': 
                     this.addOperation(parseInt(e.key));
+                    break;
+                case 'c': 
+                    if(e.ctrlKey) this.copyToClickBoard();
                     break;
             }
         });
